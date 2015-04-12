a838b6c58e9856efb0b947c91fa962b114c61fd3
Barry <bachma0507@gmail.com>
2015-04-12 13:41:03 -0400

    Changes to choiceScreen and Game Center login feature

--- testData/choicescreen.swift
+++ testData/choicescreen.swift
@@ -7,8 +7,13 @@
 //
 
 import Foundation
+import UIKit
+import GameKit
 
-class ChoiceScreen : UIViewController {
+class ChoiceScreen : UIViewController, GKGameCenterControllerDelegate {
+    
+    var gcEnabled = Bool() // Stores if the user has Game Center enabled
+    var gcDefaultLeaderBoard = String() // Stores the default leaderboardID
     
     @IBOutlet var leadPlayerButton: UIButton!
     
@@ -19,27 +24,52 @@ class ChoiceScreen : UIViewController {
         var query = PFQuery(className: "game")
         query.whereKey("player", equalTo: PFUser.currentUser())
         
-        query.getFirstObjectInBackgroundWithBlock {
-            (myGame: PFObject!, error: NSError!) -> Void in
-            if error != nil {
-                NSLog("%@", error)
-            } else {
-                 myGame["leadplayer"] = "yes"
-                myGame.saveInBackgroundWithBlock {
-                    (success: Bool, error: NSError!) -> Void in
-                    if (success) {
-                        // The object has been saved.
-                        self.performSegueWithIdentifier("leadplayer", sender: self)
-                    } else if error != nil {
-                        let errorString = "\(error)"
-                        SCLAlertView().showError("Oops...", subTitle:"There was an error: \(errorString)", closeButtonTitle:"OK")
-                        // Show the errorString somewhere and let the user try again.
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            if !(error != nil) {
+                
+                for object in objects {
+                    var myLeadPlayer: String? = object["leadplayer"] as? String
+                    
+                    if myLeadPlayer == "yes"{
+                        
+                        SCLAlertView().showError("Oops...", subTitle:"The lead player role has already been chosen, please choose Regular Player.", closeButtonTitle:"OK")
+                        
+                        self.leadPlayerButton.enabled = false
+                        
                     }
-
+                    else{
+                        
+                        query.getFirstObjectInBackgroundWithBlock {
+                            (myGame: PFObject!, error: NSError!) -> Void in
+                            if error != nil {
+                                NSLog("%@", error)
+                            } else {
+                                myGame["leadplayer"] = "yes"
+                                myGame.saveInBackgroundWithBlock {
+                                    (success: Bool, error: NSError!) -> Void in
+                                    if (success) {
+                                        // The object has been saved.
+                                        self.performSegueWithIdentifier("leadplayer", sender: self)
+                                    } else if error != nil {
+                                        let errorString = "\(error)"
+                                        SCLAlertView().showError("Oops...", subTitle:"There was an error: \(errorString)", closeButtonTitle:"OK")
+                                        // Show the errorString somewhere and let the user try again.
+                                    }
+                                    
+                                    
+                                }
+                                
+                            }
+                        }
+                        
+                        
+                    }
                     
                 }
                 
             }
+            
         }
         
         
@@ -109,11 +139,11 @@ class ChoiceScreen : UIViewController {
                 myGame["Round"] = "0"
                 myGame["seconds"] = "60"
                 myGame["leadplayer"] = "no"
-                myGame["field1"] = "empty"
-                myGame["field2"] = "empty"
-                myGame["field3"] = "empty"
-                myGame["field4"] = "empty"
-                myGame["field5"] = "empty"
+                myGame["field1"] = "boy name"
+                myGame["field2"] = "girl name"
+                myGame["field3"] = "animal"
+                myGame["field4"] = "city"
+                myGame["field5"] = "country"
                 myGame["field6"] = "empty"
                 myGame["field7"] = "empty"
                 myGame["field8"] = "empty"
@@ -154,6 +184,8 @@ class ChoiceScreen : UIViewController {
         NSUserDefaults.standardUserDefaults().removeObjectForKey("8")
         NSUserDefaults.standardUserDefaults().removeObjectForKey("9")
         NSUserDefaults.standardUserDefaults().removeObjectForKey("10")
+        
+        self.authenticateLocalPlayer()
 
         
         var query = PFQuery(className: "game")
@@ -190,6 +222,42 @@ class ChoiceScreen : UIViewController {
         
     }
     
+    func authenticateLocalPlayer() {
+        let localPlayer: GKLocalPlayer = GKLocalPlayer.localPlayer()
+        
+        localPlayer.authenticateHandler = {(ViewController, error) -> Void in
+            if((ViewController) != nil) {
+                // 1 Show login if player is not logged in
+                self.presentViewController(ViewController, animated: true, completion: nil)
+            } else if (localPlayer.authenticated) {
+                // 2 Player is already euthenticated & logged in, load game center
+                self.gcEnabled = true
+                
+                // Get the default leaderboard ID
+                localPlayer.loadDefaultLeaderboardIdentifierWithCompletionHandler({ (leaderboardIdentifer: String!, error: NSError!) -> Void in
+                    if error != nil {
+                        println(error)
+                    } else {
+                        self.gcDefaultLeaderBoard = leaderboardIdentifer
+                    }
+                })
+                
+                
+            } else {
+                // 3 Game center is not enabled on the users device
+                self.gcEnabled = false
+                println("Local player could not be authenticated, disabling game center")
+                println(error)
+            }
+            
+        }
+        
+    }
+
+    func gameCenterViewControllerDidFinish(gameCenterViewController: GKGameCenterViewController!) {
+        gameCenterViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+
     
 
 }
